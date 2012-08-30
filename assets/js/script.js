840181f26ce0741238e3bc4f4537193761f6ef21
Adam Brewer <adamcbrewer@gmail.com>
2012-08-30 22:24:20 +0100

    Now saving the persons position to the relevant profile

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,12 +25,14 @@ $(function (Site) {
 
 				this.avatar
 					.attr({
-						'class': 'person'
+						'class': 'person',
+						'data-id': profile.id,
+						'data-posx': '',
+						'data-posy': ''
 					})
 					.css({
 						'background-image': 'url('+this.profileImg+')'
 					});
-					// .html('<img src="'+this.profileImg+'" />');
 			},
 
 			_setup: function () {
@@ -72,7 +74,6 @@ $(function (Site) {
 		//
 		getProfiles: function () {
 
-			this.people = [];
 			var that = this,
 				uri = 'https://api.twitter.com/1/users/lookup.json?screen_name='+this.twitterList.join(',')+'&include_entities=true&callback=?',
 				users = [];
@@ -80,7 +81,7 @@ $(function (Site) {
 			$.getJSON(uri, function (response) {
 				users = response;
 				users.forEach(function (profile) {
-					that.people.push(new Person(profile));
+					that.profiles.push(new Person(profile));
 				});
 				that.placePeopleInOffice();
 			});
@@ -90,7 +91,7 @@ $(function (Site) {
 
 			var floor = this.floor;
 
-			this.people.forEach(function (person, i) {
+			this.profiles.forEach(function (person, i) {
 				floor.append(person.avatar);
 			});
 
@@ -107,12 +108,90 @@ $(function (Site) {
 		//
 		initDragging: function () {
 
-			this.draggable = this.floor.find('.person').draggable({
+			var that = this;
+
+			this.people = this.floor.find('.person');
+
+			this.draggable = this.people.draggable({
 				grid: [45, 45],
-				containment: 'parent'
+				containment: 'parent',
+
+				// events
+				create: function (event, ui) {
+
+				},
+				start: function (event, ui) {
+
+				},
+				drag: function (event, ui) {
+
+				},
+				stop: function (event, ui) {
+
+					that.updatePersonData(this).updateProfile(this);
+
+					// that.serialize();
+
+				}
 			}).data('draggable');
 
-			// console.log(this.draggable);
+		},
+
+		//
+		// updatePersonData
+		//
+		// Records any data we need for storing / later-use
+		// =============================
+		//
+		updatePersonData: function (el) {
+
+			var x = parseInt(el.style.top, 10),
+				y = parseInt(el.style.left, 10);
+			el.setAttribute('data-posx', x);
+			el.setAttribute('data-posy', y);
+
+			return this;
+
+		},
+
+
+
+		//
+		// updateProfile
+		//
+		// Adds the updated data to the relevant profile
+		// =============================
+		//
+		updateProfile: function (person) {
+
+			this.profiles.forEach(function (profile) {
+				if (profile.id == person.getAttribute('data-id')) {
+					profile.data = {
+						x: person.getAttribute('data-posx'),
+						y: person.getAttribute('data-posy')
+					};
+				}
+				return;
+			});
+
+			// TODO: send to the server
+
+		},
+
+
+		//
+		// serialize
+		//
+		// Returns an object witht he current state of the office and positions
+		// =============================
+		//
+		serialize: function () {
+
+			var that = this;
+
+			// $.each(this.people, function (i, person) {
+			// 	that.updateProfile(person.getAttribute('data-id'), person);
+			// });
 
 		},
 
@@ -131,6 +210,8 @@ $(function (Site) {
 			];
 
 			this.floor = $("#floor");
+			this.people = [];
+			this.profiles = [];
 
 		}
 
