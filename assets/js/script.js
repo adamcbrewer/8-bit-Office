585633a9ea48db10915dec31a9749a782a0245d0
Adam Brewer <adamcbrewer@gmail.com>
2012-08-31 07:35:08 +0100

    Improved dragging class names

--- assets/js/script.js
+++ assets/js/script.js
@@ -154,10 +154,6 @@ $(function (Site) {
 				that.placePeopleInOffice();
 			});
 
-			// $.getJSON(rageUri, function (response) {
-			// 	console.log(response);
-			// 	// that.placePeopleInOffice();
-			// });
 		},
 
 		//
@@ -195,19 +191,22 @@ $(function (Site) {
 			this.draggable = this.people.draggable({
 				grid: [45, 45],
 				containment: 'parent',
+				addClasses: false,
 
 				// events
 				create: function (event, ui) {
 
 				},
 				start: function (event, ui) {
-
+					that.people.not(this).removeClass('dragging');
+					that.people.filter(this).addClass("dragging");
 				},
 				drag: function (event, ui) {
 
 				},
 				stop: function (event, ui) {
 
+					that.people.removeClass('last-drag').filter(this).addClass('last-drag').removeClass("dragging");
 					that.updatePersonData(this).updateProfile(this);
 
 					// that.serialize();
